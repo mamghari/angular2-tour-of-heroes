b9e56ff35989e0937b13c389a7c8b641c285cdef
Colin Eberhardt <ceberhardt@scottlogic.co.uk>
2015-12-21 16:09:38 +0000

    Moved to having a distribution folder
    Benefits
     + No longer mixing ts and compiled js output
     + Makes it clearer which files are / are not used
    
    Notes:
     + tsconfig.json - updated so that command line builds write to the same folder
     + gulp-typescript does not support sourcemaps
     + Repo structure not ideal, mixing source files

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,41 @@ const gulp = require('gulp');
 const del = require('del');
 const typescript = require('gulp-typescript');
 const tscConfig = require('./tsconfig.json');
-
-const paths = {
-  dist: 'dist',
-  distFiles: 'dist/**/*',
-  srcTsFiles: 'app/**/*.ts'
-}
+const sourcemaps = require('gulp-sourcemaps');
 
 // clean the contents of the distribution directory
 gulp.task('clean', function () {
-  return del(paths.distFiles);
+  return del('dist/**/*');
 });
+
+// copy static assets - i.e. non TypeScript compiled source
+gulp.task('copy:assets', ['clean'], function() {
+  return gulp.src(['app/**/*', 'index.html', 'styles.css', '!app/**/*.ts'], { base : './' })
+    .pipe(gulp.dest('dist'))
+});
+
+// copy dependencies
+gulp.task('copy:libs', ['clean'], function() {
+  return gulp.src([
+      'node_modules/angular2/bundles/angular2-polyfills.js',
+      'node_modules/systemjs/dist/system.src.js',
+      'node_modules/rxjs/bundles/Rx.js',
+      'node_modules/angular2/bundles/angular2.dev.js',
+      'node_modules/angular2/bundles/router.dev.js'
+    ])
+    .pipe(gulp.dest('dist/lib'))
+});
+
 
 // TypeScript compile
 gulp.task('compile', ['clean'], function () {
   return gulp
-    .src(paths.srcTsFiles)
+    .src('app/**/*.ts')
+    .pipe(sourcemaps.init())
     .pipe(typescript(tscConfig.compilerOptions))
-    .pipe(gulp.dest(paths.dist));
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/app'));
 });
 
-gulp.task('build', ['compile']);
+gulp.task('build', ['compile', 'copy:libs', 'copy:assets']);
 gulp.task('default', ['build']);
